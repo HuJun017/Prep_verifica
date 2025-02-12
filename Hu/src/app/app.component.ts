302bfb82e7a41cac7cbed3d95c3f7f05679112d0
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2025-02-12 13:45:52 +0000

    modifiche in corso

--- Hu/src/app/app.component.ts
+++ Hu/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SecondoComponent } from './secondo/secondo.component';
+import { TerzoComponent } from './terzo/terzo.component';
+import { QuartoComponent } from './quarto/quarto.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SecondoComponent, TerzoComponent, QuartoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
