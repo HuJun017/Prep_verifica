847767209d813095c14e28f27f2d84b7d53d662d
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2025-02-12 13:53:58 +0000

    risolto problema dichirazioni componenti

--- Hu/src/app/app.component.ts
+++ Hu/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondoComponent } from './secondo/secondo.component';
 import { TerzoComponent } from './terzo/terzo.component';
-import { QuartoComponent } from './quarto/quarto.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SecondoComponent, TerzoComponent, QuartoComponent],
+  imports: [SecondoComponent, TerzoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
